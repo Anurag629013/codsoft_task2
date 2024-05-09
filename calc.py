dd9bcdbb7184fcf7c9503e00c4f4003000591d03
Anurag629013 <anuragkumarsingh6291@gmail.com>
2024-05-10 03:38:06 +0530

    calc.py

--- calc.py
+++ calc.py
@@ -1,22 +1,22 @@
 def add(x, y):
     return x + y
 
-def sub(x, y):
+def subtract(x, y):
     return x - y
 
-def mult(x, y):
+def multiply(x, y):
     return x * y
 
-def div(x, y):
+def division(x, y):
     return x / y
 
 while True:
-    print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")  
-    c = input("Enter choice ( 1      2       3       4 ) : ")
+    print("Select operation to be performed.\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")  
+    c = input("Enter your choice ( 1      2       3       4 ) : ")
     if c in ('1', '2', '3', '4'):
         try:
-            n1 = float(input("Enter first number: "))
-            n2 = float(input("Enter second number: "))
+            n1 = float(input("Enter the first number: "))
+            n2 = float(input("Enter the second number: "))
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
@@ -25,19 +25,19 @@ while True:
             print(n1, "+", n2, "=", add(n1, n2))
 
         elif c == '2':
-            print(n1, "-", n2, "=", sub(n1, n2))
+            print(n1, "-", n2, "=", subtract(n1, n2))
 
         elif c == '3':
-            print(n1, "*", n2, "=", mult(n1, n2))
+            print(n1, "*", n2, "=", multiply(n1, n2))
 
         elif c == '4':
             if n2 == 0:
                 print("Cannot divide by zero.")
                 continue
-            print(n1, "/", n2, "=", div(n1, n2))
+            print(n1, "/", n2, "=", division(n1, n2))
 
-        nextcal = input("Let's do next calculation? (yes/no): ")
+        nextcal = input("Want to Continue calculation? (yes/no): ")
         if (nextcal == "no") or (nextcal == "n") or (nextcal == "NO") or (nextcal == "N") or (nextcal == "No"):
             break
     else:
-        print("Invalid Input")
+        print("Invalid Input!")
